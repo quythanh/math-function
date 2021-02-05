729dbd1e18f4a1209e9a3aec1bd276924d0219d5
Quy Thanh <quythanh2205@gmail.com>
2021-02-05 21:09:23 +0700

    Update

--- polynomial.py
+++ polynomial.py
@@ -20,23 +20,23 @@ class Polynomial():
                 print(f"x{i+1} = {self.result[i]}")
 
     def Degree_1(self):
-        x = str(formatNumber(round(-self.b/self.a, 15)))
+        x = str(formatNumber(round(-self.b/self.a, 14)))
         self.result.append(x)
         self.Show()
 
     def Degree_2(self):
         delta = self.b**2 - 4*self.a*self.c
         if delta == 0:
-            x = str(formatNumber(round(-self.b/(2*self.a)), 15))
+            x = str(formatNumber(round(-self.b/(2*self.a)), 14))
             self.result.append(x)
         elif delta > 0:
-            x1 = str(formatNumber(round((-self.b+(delta)**(1/2))/(2*self.a), 15)))
-            x2 = str(formatNumber(round((-self.b-(delta)**(1/2))/(2*self.a), 15)))
+            x1 = str(formatNumber(round((-self.b+(delta)**(1/2))/(2*self.a), 14)))
+            x2 = str(formatNumber(round((-self.b-(delta)**(1/2))/(2*self.a), 14)))
             self.result.append(x1)
             self.result.append(x2)
         else:
-            real = str(formatNumber(round(-self.b/(2*self.a), 15)))
-            imagine = str(formatNumber(round((-delta)**(1/2)/(2*a), 15)))
+            real = str(formatNumber(round(-self.b/(2*self.a), 14)))
+            imagine = str(formatNumber(round((-delta)**(1/2)/(2*a), 14)))
             x1 = real + " + " + imagine + "i"
             x2 = real + " - " + imagine + "i"
             self.result.append(x1)
@@ -48,27 +48,58 @@ class Polynomial():
         k = (9*self.a*self.b*self.c-2*self.b**3-27*self.a**2*self.d)/(2*(abs(delta**3))**0.5)
         if delta > 0:
             if abs(k) < 1:
-                x1 = str(formatNumber(round((2*delta**0.5*cos(acos(k)/3)-self.b)/(3*self.a), 15)))
-                x2 = str(formatNumber(round((2*delta**0.5*cos((acos(k)-2*self.pi)/3)-self.b)/(3*self.a), 15)))
-                x3 = str(formatNumber(round((2*delta**0.5*cos((acos(k)+2*self.pi)/3)-self.b)/(3*self.a), 15)))
+                x1 = str(formatNumber(round((2*delta**0.5*cos(acos(k)/3)-self.b)/(3*self.a), 14)))
+                x2 = str(formatNumber(round((2*delta**0.5*cos((acos(k)-2*self.pi)/3)-self.b)/(3*self.a), 14)))
+                x3 = str(formatNumber(round((2*delta**0.5*cos((acos(k)+2*self.pi)/3)-self.b)/(3*self.a), 14)))
                 self.result.append(x1)
                 self.result.append(x2)
                 self.result.append(x3)
             elif abs(k) == 1:
-                x1 = str(formatNumber(round((2*delta**0.5-self.b)/(3*self.a), 15)))
-                x2 = str(formatNumber(round((-delta**0.5-self.b)/(3*self.a), 15)))
+                x1 = str(formatNumber(round((2*delta**0.5-self.b)/(3*self.a), 14)))
+                x2 = str(formatNumber(round((-delta**0.5-self.b)/(3*self.a), 14)))
                 self.result.append(x1)
                 self.result.append(x2)
             else:
-                x = str(formatNumber(round((delta**0.5*abs(k)/(3*self.a*k))*((abs(k)+(k*k-1)**0.5)**(1/3)+(abs(k)-(k*k-1)**0.5)**(1/3))-self.b/(3*self.a), 15)))
+                x = str(formatNumber(round((delta**0.5*abs(k)/(3*self.a*k))*((abs(k)+(k*k-1)**0.5)**(1/3)+(abs(k)-(k*k-1)**0.5)**(1/3))-self.b/(3*self.a), 14)))
                 self.result.append(x)
         elif delta == 0:
-            x = str(formatNumber(round((-self.b+(self.b**3-27*self.a**2*self.d)**(1/3))/(3*self.a), 15)))
+            x = str(formatNumber(round((-self.b+(self.b**3-27*self.a**2*self.d)**(1/3))/(3*self.a), 14)))
             self.result.append(x)
         else:
-            x = str(formatNumber(round((abs(delta)**0.5/(3*self.a))*((k+(k*k+1)**0.5)**(1/3)-((k*k+1)**0.5-k)**(1/3))-self.b/(3*self.a), 15)))
+            x = str(formatNumber(round((abs(delta)**0.5/(3*self.a))*((k+(k*k+1)**0.5)**(1/3)-((k*k+1)**0.5-k)**(1/3))-self.b/(3*self.a), 14)))
             self.result.append(x)
         self.Show()
+
+    def Degree_4(self):
+        # t^4 + at^2 + bt + c = 0
+        at = (-3*self.b**2+8*self.a*self.c)/(8*self.a**2)
+        bt = (2*self.b**3-8*self.a*self.b*self.c+16*self.a**2*self.d)/(16*self.a**3)
+        ct = (-3*self.b**4+16*self.a*self.b**2*self.c-64*self.a**2*self.b*self.d+256*self.a**3*self.e)/(256*self.a**4)
+
+        solve_m = Polynomial(8, -4*at, -8*ct, 4*at*ct-bt**2, 0) #8m^3 - 4am^2 - 8cm + 4ac - b^2 = 0
+        solve_m.Degree_3()
+        m = eval(solve_m.result[0])
+
+        pre_result_1 = Polynomial(1, -(2*m-at)**0.5, m + b/(2*(2*m-at)**0.5), 0, 0)
+        pre_result_2 = Polynomial(1, (2*m-at)**0.5, m - b/(2*(2*m-at)**0.5), 0, 0)
+
+        list_pre_results = []
+        for j in [pre_result_1, pre_result_2]:
+            j.Degree_2()
+            for i in j.result:
+                list_pre_results.append(i)
+
+        for i in list_pre_results:
+            if 'i' not in i:
+                x = str(formatNumber(round(eval(i) - self.b/(4*self.a), 14)))
+                self.result.append(x)
+            else:
+                classify = i.split(' ')
+                classify[0] = str(formatNumber(round(eval(classify[0]) - self.b/(4*self.a), 14)))
+                x = classify[0] + ' ' + classify[1] + ' ' + classify[2]
+                self.result.append(x)
+        self.Show()
+
 
 if __name__ == "__main__":
     a, b, c, d, e = 0, 0, 0, 0, 0
